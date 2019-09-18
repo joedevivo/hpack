988f9222352678fa09c1f903b674ae6e964eebfb
Paul J. Davis <paul.joseph.davis@gmail.com>
2019-09-18 14:17:35 -0500

    Implement proper string encoding/decoding

--- src/hpack_string.erl
+++ src/hpack_string.erl
@@ -3,26 +3,55 @@
 -module(hpack_string).
 
 -export([
-         decode/1
-        ]).
+    encode/2,
+    decode/1,
+
+    is_uncompressed/1,
+    any_uncompressed/1
+]).
+
+
+-include("hpack.hrl").
+
+
+-spec encode(binary(), [header_opt()]) -> binary().
+encode(Bin, Opts) ->
+    Uncompressed = lists:member(uncompressed, Opts),
+    {Compressed, DataBin} = case Uncompressed of
+        true ->
+            {false, Bin};
+        false ->
+            case hpack_huffman:encode(Bin) of
+                {error, compressed_larger} ->
+                    {false, Bin};
+                HBin when is_binary(HBin) ->
+                    {true, HBin}
+            end
+    end,
+    SizeBin = hpack_integer:encode(size(DataBin), 7),
+    case Compressed of
+        true -> <<1:1, SizeBin/bits, DataBin/binary>>;
+        false -> <<0:1, SizeBin/bits, DataBin/binary>>
+    end.
+
 
 -spec decode(binary()) -> {binary(), binary()}.
 decode(<<>>) ->
-    {<<>>, <<>>};
-decode(<<Huff:1,Bin/bits>>) ->
-    {Length, Rem} = hpack_integer:decode(Bin, 7),
-    <<RawLiteral:Length/binary,B2/bits>> = Rem,
-    Literal = case Huff of
-                  1 ->
-                      huffman:decode(RawLiteral);
-                  0 ->
-                      RawLiteral
-              end,
-    {Literal, B2}.
+    ?ERROR({invalid_string, no_data});
 
-%% As the Huffman-encoded data doesn't always end at an octet
-%% boundary, some padding is inserted after it, up to the next octet
-%% boundary.  To prevent this padding from being misinterpreted as
-%% part of the string literal, the most significant bits of the code
-%% corresponding to the EOS (end-of-string) symbol are used.
-%% EOS (256)  |11111111|11111111|11111111|111111      3fffffff  [30]
+decode(<<Huff:1, B1/bits>>) ->
+    {Length, B2} = hpack_integer:decode(B1, 7),
+    <<Data:Length/binary, B3/bits>> = B2,
+    Value = case Huff of
+        0 -> Data;
+        1 -> hpack_huffman:decode(Data)
+    end,
+    {Value, B3}.
+
+
+is_uncompressed(<<0:1, _/bits>>) -> true;
+is_uncompressed(<<1:1, _/bits>>) -> false.
+
+
+any_uncompressed(Bins) when is_list(Bins) ->
+    lists:any(fun is_uncompressed/1, Bins).
--- test/hpack_string_tests.erl
+++ test/hpack_string_tests.erl
@@ -1,26 +1,67 @@
+
 -module(hpack_string_tests).
+
 -include_lib("eunit/include/eunit.hrl").
--compile([export_all]).
+
 
 basic_decode_test() ->
-    %% "Commands
-    Bin = <<0:1,8:7,$C,$o,$m,$m,$a,$n,$d,$s>>,
-    ?assertEqual({<<"Commands">>, <<>>}, hpack_string:decode(Bin)),
-    ok.
+    Bin = <<0:1, 8:7, $C,$o,$m,$m,$a,$n,$d,$s>>,
+    ?assertEqual({<<"Commands">>, <<>>}, hpack_string:decode(Bin)).
+
 
 basic_decode_with_huffman_test() ->
-    %% "Commands"
-    Bin = <<1:1,
-            6:7, %% now it's 6 bytes instead of 8. Savings!
-            2#1011110:7, %% $C  7
-            2#00111:5,   %% $o  5
-            2#101001:6,  %% $m  6
-            2#101001:6,  %% $m  6
-            2#00011:5,   %% $a  5
-            2#101010:6,  %% $n  6
-            2#100100:6,  %% $d  6
-            2#01000:5,   %% $s +5
-            0:2          %%   =46
-          >>,
-    ?assertEqual({<<"Commands">>, <<>>}, hpack_string:decode(Bin)),
-    ok.
+    Bin = <<
+        1:1,
+        6:7, %% now it's 6 bytes instead of 8. Savings!
+        2#1011110:7, %% $C  7
+        2#00111:5,   %% $o  5
+        2#101001:6,  %% $m  6
+        2#101001:6,  %% $m  6
+        2#00011:5,   %% $a  5
+        2#101010:6,  %% $n  6
+        2#100100:6,  %% $d  6
+        2#01000:5,   %% $s +5
+        2#11:2       %%   =46
+    >>,
+    ?assertEqual({<<"Commands">>, <<>>}, hpack_string:decode(Bin)).
+
+
+short_string_uncompressed_test() ->
+    % 302 can compress into two bytes
+    ?assertEqual(<<130, 100, 2>>, hpack_string:encode(<<"302">>, [])),
+    % 307 takes three bytes which is equivalent
+    % to the uncompressed versions so that's
+    % what we use.
+    ?assertEqual(<<3, "307">>, hpack_string:encode(<<"307">>, [])).
+
+
+empty_binary_test() ->
+    ?assertThrow(
+            {hpack_error, {invalid_string, no_data}},
+            hpack_string:decode(<<>>)
+        ).
+
+
+roundtrip_sequential_test() ->
+    lists:foreach(fun(IntValue) ->
+        roundtrip(integer_to_binary(IntValue), []),
+        roundtrip(integer_to_binary(IntValue), [uncompressed])
+    end, lists:seq(0, 512)).
+
+
+roundtrip_random_test() ->
+    lists:foreach(fun(_) ->
+        Value = hpack_tutil:random_bin(256),
+        roundtrip(Value, []),
+        roundtrip(Value, [uncompressed])
+    end, lists:seq(0, 512)).
+
+
+roundtrip(Value, Opts) ->
+    Encoded = hpack_string:encode(Value, Opts),
+    Decoded = hpack_string:decode(Encoded),
+    ?assertEqual({Value, <<>>}, Decoded),
+
+    Garbage = hpack_tutil:random_bin(64),
+    WithGarbage = <<Encoded/binary, Garbage/binary>>,
+    ?assertEqual({Value, Garbage}, hpack_string:decode(WithGarbage)).
