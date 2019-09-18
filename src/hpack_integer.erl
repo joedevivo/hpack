f1efbf5b48f9be778683ff1ae2b7493d274f6f0c
Paul J. Davis <paul.joseph.davis@gmail.com>
2019-09-18 14:13:12 -0500

    Simplify integer encoding/decoding

--- src/hpack_integer.erl
+++ src/hpack_integer.erl
@@ -6,101 +6,59 @@
 %% as defined http://tools.ietf.org/html/rfc7541#section-5.1
 
 -export([
-         decode/2,
-         encode/2
-        ]).
+    encode/2,
+    decode/2
+]).
 
-%% To save every bit on the wire, an integer can fill a partial octet,
-%% and usually does in HPACK. If that integer is small enough to fit,
-%% then no further work is required.
 
-%% In the octet below, the first three bits were used for something
-%% else. Doesn't matter what. What matters is that we have 5 bits left
-%% for the integer, which means if it's less than 2^5-1, we're all
-%% good!
+-spec encode(non_neg_integer(), pos_integer()) -> bitstring().
+encode(  1, 1) -> <<  1:1, 0:8>>;
+encode(  3, 2) -> <<  3:2, 0:8>>;
+encode(  7, 3) -> <<  7:3, 0:8>>;
+encode( 15, 4) -> << 15:4, 0:8>>;
+encode( 31, 5) -> << 31:5, 0:8>>;
+encode( 63, 6) -> << 63:6, 0:8>>;
+encode(127, 7) -> <<127:7, 0:8>>;
+encode(255, 8) -> <<255:8, 0:8>>;
 
-%%    0   1   2   3   4   5   6   7
-%%   +---+---+---+---+---+---+---+---+
-%%   | ? | ? | ? |       Value       |
-%%   +---+---+---+-------------------+
-
-%% So what if it's greater than 2^5-1?
-
-%%     0   1   2   3   4   5   6   7
-%%   +---+---+---+---+---+---+---+---+
-%%   | ? | ? | ? | 1   1   1   1   1 |
-%%   +---+---+---+-------------------+
-%%   | 1 |    Value-(2^5-1) LSB      |
-%%   +---+---------------------------+
-%%                  ...
-%%   +---+---------------------------+
-%%   | 0 |    Value-(2^5-1) MSB      |
-%%   +---+---------------------------+
-
-%% Then we don't want to waste those first 5 bits, so they're all set
-%% to 1, and we know the integer is greater than 2^5-1. How much
-%% greater? That's the question we answer with the following octets.
-
-%% "Value-(2^5-1)" is "how much greater?". Let's call it I.
-
--spec decode(binary(), pos_integer()) -> {non_neg_integer(), binary()}.
-decode(<<1:1,Bin/bits>>, 1) ->
-    prefix_plus(1, decode(Bin, 0, 0));
-decode(<<3:2,Bin/bits>>, 2) ->
-    prefix_plus(3, decode(Bin, 0, 0));
-decode(<<7:3,Bin/bits>>, 3) ->
-    prefix_plus(7, decode(Bin, 0, 0));
-decode(<<15:4,Bin/bits>>, 4) ->
-    prefix_plus(15, decode(Bin, 0, 0));
-decode(<<31:5,Bin/bits>>, 5) ->
-    prefix_plus(31, decode(Bin, 0, 0));
-decode(<<63:6,Bin/bits>>, 6) ->
-    prefix_plus(63, decode(Bin, 0, 0));
-decode(<<127:7,Bin/bits>>, 7) ->
-    prefix_plus(127, decode(Bin, 0, 0));
-decode(<<255:8,Bin/bits>>, 8) ->
-    prefix_plus(255, decode(Bin, 0, 0));
-%% This clause means we have something small enough to fit in prefix
-decode(Bin, Prefix) ->
-    <<Value:Prefix,Rem/bits>> = Bin,
-    {Value, Rem}.
-
--spec decode(binary(), non_neg_integer(), non_neg_integer()) -> {non_neg_integer(), binary()}.
-decode(<<1:1,Int:7,Rem/binary>>, M, I) ->
-    decode(Rem, M+7, round(I + Int * math:pow(2, M)));
-decode(<<0:1,Int:7,Rem/binary>>, M, I) ->
-    {round(I + Int * math:pow(2, M)), Rem}.
-
--spec prefix_plus(pos_integer(), {non_neg_integer(), binary()}) -> {non_neg_integer(), binary()}.
-prefix_plus(Prefix, {I, Rem}) ->
-    {Prefix+I, Rem}.
-
--spec encode(non_neg_integer(), pos_integer()) -> binary().
-%% First clauses are peformance optimizations for Int == 2^Prefix - 1
-encode(  1,1) -> <<  1:1,0:8>>;
-encode(  3,2) -> <<  3:2,0:8>>;
-encode(  7,3) -> <<  7:3,0:8>>;
-encode( 15,4) -> << 15:4,0:8>>;
-encode( 31,5) -> << 31:5,0:8>>;
-encode( 63,6) -> << 63:6,0:8>>;
-encode(127,7) -> <<127:7,0:8>>;
-encode(255,8) -> <<255,0>>;
-encode(Int, N) when Int < (1 bsl N - 1) ->
+encode(Int, N) when is_integer(Int), is_integer(N), Int < (1 bsl N - 1) ->
     <<Int:N>>;
-encode(Int, N) ->
+
+encode(Int, N) when is_integer(Int), is_integer(N) ->
     Prefix = 1 bsl N - 1,
     Remaining = Int - Prefix,
-    Bin = encode_(Remaining, <<>>),
-    <<Prefix:N, Bin/binary>>.
+    encode_int(Remaining, [<<Prefix:N>>]).
 
--spec encode_(non_neg_integer(), binary()) -> binary().
-encode_(I, BinAcc) ->
-    LeastSigSeven = (I rem 128),
-    RestToEncode = I bsr 7,
-    case RestToEncode of
+-spec encode_int(non_neg_integer(), [bitstring()]) -> bitstring().
+encode_int(Int, Acc) when is_integer(Int), is_list(Acc) ->
+    ThisByte = (Int rem 128),
+    RestInt = Int bsr 7,
+    case RestInt of
         0 ->
-            <<BinAcc/binary, LeastSigSeven>>;
-        _ -> %% Adds the continuation bit
-            ThisByte = 128 + LeastSigSeven,
-            encode_(RestToEncode, <<BinAcc/binary, ThisByte>>)
+            list_to_bitstring(lists:reverse(Acc, [<<ThisByte:8>>]));
+        _ ->
+            encode_int(RestInt, [<<1:1, ThisByte:7>> | Acc])
     end.
+
+
+-spec decode(bitstring(), pos_integer()) -> {non_neg_integer(), binary()}.
+decode(<<  1:1, Bin/bits>>, 1) -> decode(Bin, 1,   1);
+decode(<<  3:2, Bin/bits>>, 2) -> decode(Bin, 1,   3);
+decode(<<  7:3, Bin/bits>>, 3) -> decode(Bin, 1,   7);
+decode(<< 15:4, Bin/bits>>, 4) -> decode(Bin, 1,  15);
+decode(<< 31:5, Bin/bits>>, 5) -> decode(Bin, 1,  31);
+decode(<< 63:6, Bin/bits>>, 6) -> decode(Bin, 1,  63);
+decode(<<127:7, Bin/bits>>, 7) -> decode(Bin, 1, 127);
+decode(<<255:8, Bin/bits>>, 8) -> decode(Bin, 1, 255);
+decode(Bin, Prefix) ->
+    %% This clause means we have something small
+    %% enough to fit in prefix
+    <<Value:Prefix, Rem/bits>> = Bin,
+    {Value, Rem}.
+
+
+decode(<<1:1, Int:7, Rest/binary>>, Factor, Value) ->
+    decode(Rest, Factor bsl 7, Value + (Int * Factor));
+
+decode(<<0:1, Int:7, Rest/binary>>, Factor, Value) ->
+    {Value + (Int * Factor), Rest}.
--- test/hpack_integer_tests.erl
+++ test/hpack_integer_tests.erl
@@ -1,51 +1,85 @@
 -module(hpack_integer_tests).
+
 -include_lib("eunit/include/eunit.hrl").
--compile([export_all]).
 
-decode_zero_test() ->
-    ?assertEqual({0, <<>>}, hpack_integer:decode(<<0:5>>, 5)),
-    ?assertEqual({0, <<1>>}, hpack_integer:decode(<<0:5,1:8>>, 5)),
-    ok.
 
-decode_sixtytwo_test() ->
-    ?assertEqual({62, <<>>}, hpack_integer:decode(<<62:6>>, 6)),
-    ?assertEqual({62, <<1>>}, hpack_integer:decode(<<62:6,1:8>>, 6)),
-    ok.
+encode_decode_simple_test() ->
+    % Value, PrefixBits, Encoded
+    Tests = [
+        % Basic tests
+        {0, 5, <<0:5>>},
+        {62, 6, <<62:6>>},
+        {63, 6, <<63:6, 0:8>>},
+        {64, 6, <<63:6, 1:8>>},
 
-decode_sixtyfour_test() ->
-    ?assertEqual({64, <<>>}, hpack_integer:decode(<<63:6,1:8>>, 6)),
-    ?assertEqual({64, <<1>>}, hpack_integer:decode(<<63:6,1:8,1:8>>, 6)),
-    ok.
+        % Three byte encoding
+        {267, 7, <<127:7, 140:8, 1:8>>},
 
-encode_integer_test() ->
-    ?assertEqual(<<62:6>>, hpack_integer:encode(62,6)),
-    ?assertEqual(<<63:6,1:8>>, hpack_integer:encode(64,6)),
-    ?assertEqual(<<63:6,0:8>>, hpack_integer:encode(63,6)),
-    ok.
+        % Max value for prefix
+        {1, 1, <<1:1, 0:8>>},
+        {3, 2, <<3:2, 0:8>>},
+        {7, 3, <<7:3, 0:8>>},
+        {15, 4, <<15:4, 0:8>>},
+        {31, 5, <<31:5, 0:8>>},
+        {63, 6, <<63:6, 0:8>>},
+        {127, 7, <<127:7, 0:8>>},
+        {255, 8, <<255:8, 0:8>>},
 
-encode_prefix_max_equals_value_test() ->
-    ?assertEqual(<<  1:1,0:8>>, hpack_integer:encode(  1,1)),
-    ?assertEqual(<<  3:2,0:8>>, hpack_integer:encode(  3,2)),
-    ?assertEqual(<<  7:3,0:8>>, hpack_integer:encode(  7,3)),
-    ?assertEqual(<< 15:4,0:8>>, hpack_integer:encode( 15,4)),
-    ?assertEqual(<< 31:5,0:8>>, hpack_integer:encode( 31,5)),
-    ?assertEqual(<< 63:6,0:8>>, hpack_integer:encode( 63,6)),
-    ?assertEqual(<<127:7,0:8>>, hpack_integer:encode(127,7)),
-    ?assertEqual(<<255,0>>,     hpack_integer:encode(255,8)),
-    ok.
+        % Zero for each prefix length
+        {0, 1, <<0:1>>},
+        {0, 2, <<0:2>>},
+        {0, 3, <<0:3>>},
+        {0, 4, <<0:4>>},
+        {0, 5, <<0:5>>},
+        {0, 6, <<0:6>>},
+        {0, 7, <<0:7>>},
+        {0, 8, <<0:8>>}
+    ],
+    lists:foreach(fun({Value, PrefixBits, Encoded}) ->
+        ?assertEqual(Encoded, hpack_integer:encode(Value, PrefixBits)),
+        ?assertEqual({Value, <<>>}, hpack_integer:decode(Encoded, PrefixBits))
+    end, Tests).
 
-encode_zero_test() ->
-    ?assertEqual(<<0:1>>, hpack_integer:encode(0,1)),
-    ?assertEqual(<<0:2>>, hpack_integer:encode(0,2)),
-    ?assertEqual(<<0:3>>, hpack_integer:encode(0,3)),
-    ?assertEqual(<<0:4>>, hpack_integer:encode(0,4)),
-    ?assertEqual(<<0:5>>, hpack_integer:encode(0,5)),
-    ?assertEqual(<<0:6>>, hpack_integer:encode(0,6)),
-    ?assertEqual(<<0:7>>, hpack_integer:encode(0,7)),
-    ?assertEqual(<<0>>,   hpack_integer:encode(0,8)),
-    ok.
 
-mplus7_test() ->
-    ?assertEqual(<<127:7,140,1>>, hpack_integer:encode(267, 7)),
-    ?assertEqual({267, <<>>}, hpack_integer:decode(<<127:7,140,1>>, 7)),
-    ok.
+decode_with_tail_test() ->
+    Tests = [
+        % Prefix bits, encoded, decoded
+        {5, <<0:5, 1:8>>, {0, <<1:8>>}},
+        {6, <<62:6, 1:8>>, {62, <<1:8>>}},
+        {6, <<63:6, 0:8, 1:8>>, {63, <<1:8>>}},
+        {6, <<63:6, 1:8, 1:8>>, {64, <<1:8>>}}
+    ],
+
+    lists:foreach(fun({PrefixBits, Encoded, Decoded}) ->
+        ?assertEqual(Decoded, hpack_integer:decode(Encoded, PrefixBits))
+    end, Tests).
+
+
+roundtrip_sequential_test() ->
+    lists:foreach(fun(Value) ->
+        lists:foreach(fun(PrefixBits) ->
+            roundtrip(Value, PrefixBits)
+        end, lists:seq(1, 8))
+    end, lists:seq(0, 512)).
+
+
+roundtrip_random_test() ->
+    lists:foreach(fun(_) ->
+        Value = hpack_tutil:random_int(16#FFFF),
+        lists:foreach(fun(PrefixBits) ->
+            roundtrip(Value, PrefixBits)
+        end, lists:seq(1, 8))
+    end, lists:seq(0, 512)).
+
+
+roundtrip(Value, PrefixBits) ->
+    Encoded = hpack_integer:encode(Value, PrefixBits),
+    Decoded = hpack_integer:decode(Encoded, PrefixBits),
+    ?assertEqual({Value, <<>>}, Decoded),
+
+    Garbage = hpack_tutil:random_bin(64),
+    WithGarbage = <<Encoded/bits, Garbage/binary>>,
+    ?assertEqual(
+        {Value, Garbage},
+        hpack_integer:decode(WithGarbage, PrefixBits)
+    ).
